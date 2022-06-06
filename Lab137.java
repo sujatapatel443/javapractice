b831252bac60bc0ae753317c0e8b999c971fea33
sujatapatel443 <106990347+sujatapatel443@users.noreply.github.com>
2022-06-06 22:26:42 +0530

    Add files via upload
    added file by dragon drop

---
+++ Lab137.java
@@ -0,0 +1,13 @@
+class Lab137{
+public static void main(String arg[]){
+int a=99;
+if(a==99){
+  int b =22;
+  System.out.println(b);
+  System.out.println(a);
+}
+System.out.println(a);
+//System.out.println(b);
+}
+
+}
